2a5c63f4beaa6e84f57a468145f62fca41d35e20
Sergio Ruiz de León <ruizdeleonsergio8@gmail.com>
2025-04-12 13:30:26 +0200

    Iniciando el programa
    Creando estructura de datos dinámica, entrada de usuario e iniciando bucle

--- src/Ciudades.java
+++ src/Ciudades.java
@@ -1,9 +1,30 @@
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
 
 public class Ciudades {
 
   public static void main(String[] args) {
 
+    // Creamos la colección para almacenar las ciudades
+    Set<String> ciudades = new HashSet<String>();
+
+    // Creamos la clase scanner
+    Scanner scanner = new Scanner(System.in);
+    String entradaUsuario; // entrada de texto introducida por el usuario
+    int opcion; // opción elegida por el usuario
+
+    // Saludamos al usuario
+    System.out.println("--- Bienvenido ---");
+
+    // Iniciamos el bucle del programa
+    while (true) {
+      System.out.println("--- OPCIONES ---");
+      break;
+    }
 
   }
 }
